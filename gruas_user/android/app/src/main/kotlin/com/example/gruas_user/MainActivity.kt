8f30aaf6748fa8f090a16645f1fcfe5c23c1d941
takumidev19 <151186342+takumidev19@users.noreply.github.com>
2025-05-08 12:34:16 -0400

    chore: estructura inicial del proyecto

---
+++ gruas_user/android/app/src/main/kotlin/com/example/gruas_user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gruas_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ gruas_worker/android/app/src/main/kotlin/com/example/gruas_worker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gruas_worker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
